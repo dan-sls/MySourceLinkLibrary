1984556bce010e0c60625be74c07ab4bddc5f988
Dan Clarke <dan.clarke@sharp-ls.eu>
2018-10-01 10:35:18 +0100

    Initial commit

---
+++ MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySourceLinkLibrary
+{
+    public class MyClass
+    {
+        public void MyFunction()
+        {
+            var a = new Random().Next();
+
+            Console.WriteLine($"Random number = {a}");
+        }
+    }
+}
